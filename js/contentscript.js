040fce9dc4b5938c1ed6ae9202fb22351f1da2ac
Jon Pierce-Ruhland <jon.pierce-ruhland@garmin.com>
2015-10-13 23:13:56 -0500

    First commit - feature complete but could optimize throttling recalculation

---
+++ js/contentscript.js
@@ -0,0 +1,79 @@
+var titleElement;
+var lastScrolledTime;
+
+function setup() {
+  var element;
+  for (element in document.all) {
+    if (element.style) {
+      element.style.position="relative";
+      element.style.zIndex="-1";
+    }
+  }
+
+  var iDiv = document.createElement('div');
+  iDiv.id = 'headless-container';
+  iDiv.className = 'headless-container';
+  var parent = document.getElementsByTagName('body')[0]
+  parent.insertBefore(iDiv, parent.children[0]);
+
+  var innerDiv = document.createElement('div');
+  innerDiv.className = 'headless';
+  innerDiv.innerHTML = 'Loading...';
+
+  iDiv.appendChild(innerDiv);
+  titleElement = innerDiv;
+}
+
+//Slow down calls to refreshText();
+function scrolled() {
+  var now = new Date().getTime();
+  if (!lastScrolledTime || lastScrolledTime < now - 250) {
+    lastScrolledTime = now;
+    refreshText();
+  }
+}
+
+function refreshText() {
+  console.log("Refreshing.");
+  var candidate;
+  var junk;
+  var i = 0;
+  for (junk in document.getElementsByClassName('differential-file-icon-header')) {
+    element = document.getElementsByClassName('differential-file-icon-header')[i];
+    if (elementAboveViewport(element)) {
+      candidate = element;
+    }
+    i += 1;
+  }
+
+  if (candidate) {
+    titleElement.style.display = 'block';
+    titleElement.innerHTML = candidate.innerHTML;
+  }
+  else {
+    titleElement.style.display = 'none';
+  }
+}
+
+function elementAboveViewport(el) {
+  if (!el) {
+    return false;
+  }
+  var top = el.offsetTop;
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+  }
+  return top <= window.pageYOffset;
+}
+
+chrome.storage.local.get({
+    url: 'http://your-url-here',
+  }, function(items) {
+    console.log("Got a url of " + items.url);
+    if (document.location.origin.match(items.url)) {
+      setup();
+      window.onscroll = scrolled;
+    }
+});
+
---
+++ js/options.js
@@ -0,0 +1,26 @@
+// Saves options to chrome.storage
+function save_options() {
+  var url = document.getElementById('url').value;
+  chrome.storage.local.set({
+    url: url,
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores options found in chrome.storage
+function restore_options() {
+  chrome.storage.local.get({
+    url: 'http://your-url-here/',
+  }, function(items) {
+    document.getElementById('url').value = items.url;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
